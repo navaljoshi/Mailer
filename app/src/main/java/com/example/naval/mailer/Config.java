9adbeb5d1f7a55cd877ef644a88c9f7da0a1aca0
naval <naval@navals-MacBook-Air.local>
2017-03-09 17:08:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/naval/mailer/Config.java
@@ -0,0 +1,10 @@
+package com.example.naval.mailer;
+
+/**
+ * Created by naval on 09/03/17.
+ */
+
+public class Config {
+    public static final String EMAIL ="navaljosh@gmail.com";
+    public static final String PASSWORD ="sanmacs88";
+}
