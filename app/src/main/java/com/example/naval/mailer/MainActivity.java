604c1f0863bb0d22aa01d04a08aa46b24811a023
naval <navaljosh@gmail.com>
2017-03-13 18:41:06 +0530

    more changes

--- app/src/main/java/com/example/naval/mailer/MainActivity.java
+++ app/src/main/java/com/example/naval/mailer/MainActivity.java
@@ -3,7 +3,18 @@ package com.example.naval.mailer;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
 import android.os.FileObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,17 +35,19 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Properties;
 
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 import static android.view.View.GONE;
+import static android.view.View.generateViewId;
 
 public class MainActivity extends AppCompatActivity  {
 
     //variable Declarations
-    String  newFile =  null;
+    File  newFile =  null;
 
     //Declaring EditText
     public EditText editTextEmail;
@@ -50,10 +63,12 @@ public class MainActivity extends AppCompatActivity  {
     public Button fbButton ;
     public LinearLayout gallery  ;
     public LinearLayout social  ;
+    public ImageView home  ;
+    public ImageView thumbnail1  ;
+    public ImageView thumbnail2  ;
+    public ImageView thumbnail3  ;
 
-
-
-
+    FileObserver observer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +83,23 @@ public class MainActivity extends AppCompatActivity  {
         fbButton = (Button) findViewById(R.id.btFacebook);
         gallery  = (LinearLayout) findViewById(R.id.linGal);
         social  = (LinearLayout) findViewById(R.id.linSocial);
+        home = (ImageView)findViewById(R.id.imgHome);
+        thumbnail1 = (ImageView)findViewById(R.id.img1);
+        thumbnail2 = (ImageView)findViewById(R.id.img2);
+        thumbnail3 = (ImageView)findViewById(R.id.img3);
+
+        haveNetworkConnection();//check is net is there?
+
+
+        observer(); // start observing the pics folder
 
         // Applying Event listners on Buttons
+
+
+        //home.setImageBitmap(bmImg);
+       // thumbnail1.setImageBitmap(bmImg);
+       // thumbnail2.setImageBitmap(bmImg);
+       // thumbnail3.setImageBitmap(bmImg);
 
         shareButton.setOnClickListener(new View.OnClickListener() {
 
@@ -112,8 +142,33 @@ public class MainActivity extends AppCompatActivity  {
 
             }
         });
-    }
 
+        // ThumbNail click listners
+
+        thumbnail1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Your code.
+                setDrawableImage("2nik.jpg");
+            }
+        });
+
+        thumbnail2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Your code.
+                setDrawableImage("3nik.jpg");
+            }
+        });
+
+
+        thumbnail3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Your code.
+                setDrawableImage("5nik.jpg");
+            }
+        });
+
+
+    }
 
     // fucntion to move from Home - FB&GMAIL sharing screen
     void sharingScreen()
@@ -123,6 +178,18 @@ public class MainActivity extends AppCompatActivity  {
         social.setVisibility(View.VISIBLE); //social buttons enabled
         fbButton.setVisibility(View.VISIBLE);
         mailButton.setVisibility(View.VISIBLE);
+    }
+
+    // fucntion to move from Home - FB&GMAIL sharing screen
+    void homeScreen()
+    {
+        shareButton.setVisibility(View.VISIBLE);
+        SharedImg.setVisibility(View.GONE);
+        //gallery.setVisibility(GONE); // gallery button disabled
+        //social.setVisibility(View.VISIBLE); //social buttons enabled
+       // fbButton.setVisibility(View.VISIBLE);
+       // mailButton.setVisibility(View.VISIBLE);
+
     }
     // fucntion to move from sharing screen FB&GMAIL to Shared successfull screen
     void successSharedScreen() {
@@ -134,6 +201,17 @@ public class MainActivity extends AppCompatActivity  {
 
         //enable shared iamge picture
         SharedImg.setVisibility(View.VISIBLE);
+        findViewById(android.R.id.content).invalidate();
+
+        try{
+            Thread.sleep(5000);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        homeScreen();
+
     }
 
 
@@ -148,32 +226,103 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+public void setDrawableImage(String path)
+{
+    String imagePath = Environment.getExternalStorageDirectory().toString()+"/nikalodean/"+path;
+    Log.d("LVMH","called getDrawableImage"+ imagePath);
+    Drawable homeDrwable = Drawable.createFromPath(imagePath);
+    home.setImageDrawable(homeDrwable);
+    home.setVisibility(View.VISIBLE);
+    home.refreshDrawableState();
+}
+
 
     // function for file observer
-    public String observer ()
-    {
+    public void observer() {
 
-
-        FileObserver observer = new FileObserver("/storage/emulated/0/nikalodean/") {
-            // set up a file observer to watch this directory on sd card
+        FileObserver fobsv = new FileObserver("/storage/emulated/0/nikalodean/") {
 
             @Override
-            public void onEvent(int event, String file) {
-                Log.d("LVMH", "File created [" + "/storage/emulated/0/nikalodean/" + file + "]");
+            public void onEvent(int event, String path) {
+                if(event == CREATE)
+                Log.d("LVMH",path);
+                setDrawableImage(path);
+                findViewById(android.R.id.content).invalidate();
 
-
-                Toast.makeText(getBaseContext(), file + " was saved!", Toast.LENGTH_LONG).show();
-
-                newFile = "/storage/emulated/0/nikalodean/"+file.toString();
 
             }
         };
-        observer.startWatching(); //START OBSERVING
+        fobsv.startWatching();
 
-        return newFile;
+    }
+
+    private Bitmap ProcessingBitmap(){
+        Bitmap bm1 ;
+        Bitmap newBitmap = null;
+
+        try {
+            bm1 =  BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                    R.drawable.shared);
+
+
+            int w = bm1.getWidth();
+
+            int h = bm1.getHeight();
+
+
+            Bitmap.Config config = bm1.getConfig();
+            if(config == null){
+                config = Bitmap.Config.ARGB_8888;
+            }
+
+            newBitmap = Bitmap.createBitmap(w, h, config);
+            Canvas newCanvas = new Canvas(newBitmap);
+
+            newCanvas.drawBitmap(bm1, 0, 0, null);
+
+            Paint paint = new Paint();
+            paint.setAlpha(128);
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return newBitmap;
+    }
+
+    void checkInternetSpeed()
+    {
+        //WifiInfo wifiInfo = WifiManager.getConnectionInfo();
+
+        //int speedMbps = wifiInfo.getLinkSpeed();
+
+       // Log.d("LVMH","Internet speed in MBPS = " +speedMbps);
     }
 
 
+    public boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected()) {
+                    haveConnectedWifi = true;
+                    Log.d("LVMH","Mobile Internet present");
+                }
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected()) {
+                    haveConnectedMobile = true;
+                    Log.d("LVMH","Mobile Internet present");
+
+                }
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
 
     public  void sendEmail() {
         final Dialog dialog1 = new Dialog(this);
@@ -224,12 +373,10 @@ public class MainActivity extends AppCompatActivity  {
                 //Executing send mail to send email
                 sm.execute();
 
-                dialog1.dismiss();// dismiss dialof and remove buttons
+                dialog1.dismiss();// dismiss dialog and remove buttons
 
 
                 successSharedScreen();
-
-
 
 
             }
